48fc3c50ff94159f4cd2340302dbc1507f811555
ethanmdavidson <ethanmdavidson@gmail.com>
2017-08-13 18:09:23 -0400

    fixed spaces to beautiful tabs, got it to bluescreen

--- app/src/main/java/com/machinerychorus/lifeprogresswallpaper/LifeWallpaper.java
+++ app/src/main/java/com/machinerychorus/lifeprogresswallpaper/LifeWallpaper.java
@@ -1,78 +1,75 @@
 package com.machinerychorus.lifeprogresswallpaper;
 
-import android.app.Service;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Canvas;
-import android.os.IBinder;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.service.wallpaper.WallpaperService;
 import android.view.SurfaceHolder;
 
 public class LifeWallpaper extends WallpaperService {
-    Context context;
-    private boolean visible;
-    Canvas canvas;
+	Context context;
 
-    public LifeWallpaper() {
-    }
+	public LifeWallpaper() {
+	}
 
-    @Override
-    public Engine onCreateEngine() {
-        context = this;
-        return new WallpaperEngine();
-    }
+	@Override
+	public Engine onCreateEngine() {
+		context = this;
+		return new WallpaperEngine();
+	}
 
-    public class WallpaperEngine extends Engine{
+	public class WallpaperEngine extends Engine{
 
-        //Called when the surface is created
-        @Override
-        public void onSurfaceCreated(SurfaceHolder holder) {
-            super.onSurfaceCreated(holder);
+		//Called when the surface is created
+		@Override
+		public void onSurfaceCreated(SurfaceHolder holder) {
+			super.onSurfaceCreated(holder);
 
-            //call the draw method
-            // this is where you must call your draw code
-            drawFrame();
+			//call the draw method
+			// this is where you must call your draw code
+			drawFrame();
 
-        }
+		}
+		
+		@Override
+		public void onVisibilityChanged(boolean visible) {
+			if (visible) {
 
-        //called when varaible changed
-        @Override
-        public void onVisibilityChanged(boolean visible) {
-            visible = visible;
-            if (visible) {
+				//call the drawFunction
+				drawFrame();
 
-                //call the drawFunction
-                drawFrame();
+			}
+		}
 
-            }
-        }
+		//called when surface destroyed
+		@Override
+		public void onSurfaceDestroyed(SurfaceHolder holder) {
+			super.onSurfaceDestroyed(holder);
+		}
 
-        //called when surface destroyed
-        @Override
-        public void onSurfaceDestroyed(SurfaceHolder holder) {
-            super.onSurfaceDestroyed(holder);
-            visible = false;
-        }
+		public void drawFrame()
+		{
+			//getting the surface holder
+			final SurfaceHolder holder = getSurfaceHolder();
 
-        public void drawFrame()
-        {
-            //getting the surface holder
-            final SurfaceHolder holder = getSurfaceHolder();
+			Canvas canvas = null;  // canvas
+			try {
+				canvas = holder.lockCanvas();  //get the canvas
+				if (canvas != null) {
+					Paint paint = new Paint();
+					paint.setColor(Color.parseColor("blue"));
 
-            canvas = null;  // canvas
-            try {
-                canvas = holder.lockCanvas();  //get the canvas
-                if (canvas != null) {
-                    // draw something
-                    // my draw code
-
-                }
-            } finally {
-                if (canvas != null)
-                    holder.unlockCanvasAndPost(canvas);
-            }
-        }
-    }
+					//paint.setAlpha(250);
+					paint.setStyle(Paint.Style.FILL);
+					canvas.drawRect(0, 0, getDesiredMinimumWidth(), getDesiredMinimumHeight(), paint);//draw rect to clear the canvas
+				}
+			} finally {
+				if (canvas != null)
+					holder.unlockCanvasAndPost(canvas);
+			}
+		}
+	}
 
 
 }
