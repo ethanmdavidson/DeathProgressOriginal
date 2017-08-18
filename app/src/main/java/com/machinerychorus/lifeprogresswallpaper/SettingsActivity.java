9ab26308594994464c6e7fb8475627e10759eefa
ethanmdavidson <ethanmdavidson@gmail.com>
2017-08-17 20:15:37 -0400

    added button to set wallpaper from inside app

--- app/src/main/java/com/machinerychorus/lifeprogresswallpaper/SettingsActivity.java
+++ app/src/main/java/com/machinerychorus/lifeprogresswallpaper/SettingsActivity.java
@@ -1,10 +1,17 @@
 package com.machinerychorus.lifeprogresswallpaper;
 
 
+import android.app.WallpaperManager;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 public class SettingsActivity extends PreferenceActivity {
 
@@ -13,6 +20,23 @@ public class SettingsActivity extends PreferenceActivity {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences);
+
+        ListView v = getListView();
+        Button setWallpaperButton = new Button(this);
+        setWallpaperButton.setText(R.string.setWallpaperButtonName);
+        setWallpaperButton.setLayoutParams(new AbsListView.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, AbsListView.LayoutParams.MATCH_PARENT));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            setWallpaperButton.setLayoutDirection(AbsListView.LAYOUT_DIRECTION_RTL);
+        }
+        setWallpaperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER);
+                startActivity(intent);
+            }
+        });
+        v.addHeaderView(setWallpaperButton);
     }
 
     /**
