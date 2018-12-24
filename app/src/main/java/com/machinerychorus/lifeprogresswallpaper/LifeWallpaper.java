ced75566c40643635546d33f061301148c3f2b4d
ethanmdavidson <ethanmdavidson@gmail.com>
2018-12-24 01:15:14 -0500

    Allow users to put text on the wallpaper

--- app/src/main/java/com/machinerychorus/lifeprogresswallpaper/LifeWallpaper.java
+++ app/src/main/java/com/machinerychorus/lifeprogresswallpaper/LifeWallpaper.java
@@ -3,16 +3,19 @@ package com.machinerychorus.lifeprogresswallpaper;
 import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.preference.PreferenceManager;
 import android.service.wallpaper.WallpaperService;
 import android.support.v4.content.ContextCompat;
 import android.view.SurfaceHolder;
+import android.view.Window;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
 import org.joda.time.DateTime;
 import org.joda.time.Hours;
 
+import java.util.List;
 import java.util.Locale;
 
 public class LifeWallpaper extends WallpaperService {
@@ -35,7 +38,7 @@ public class LifeWallpaper extends WallpaperService {
 		private long lastDrawTime;
 
 		WallpaperEngine(){
-            setOffsetNotificationsEnabled(false);
+			setOffsetNotificationsEnabled(false);
 			PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).registerOnSharedPreferenceChangeListener(this);
 		}
 
@@ -76,28 +79,44 @@ public class LifeWallpaper extends WallpaperService {
 				canvas = getSurfaceHolder().lockCanvas();
 				if (canvas != null) {
 					SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    DateTime birthdate = DateTime.parse(pref.getString(getString(R.string.birthdateKey), "1994"));
+					DateTime birthdate = DateTime.parse(pref.getString(getString(R.string.birthdateKey), "1994"));
 
-                    float percentDead = ((float) Hours.hoursBetween(birthdate, new DateTime()).getHours()) /
-							((float)Hours.hoursBetween(birthdate, birthdate.plusYears(Integer.parseInt(pref.getString(getString(R.string.expectancyKey), "85")))).getHours());
+					float hoursAlive = (float) Hours.hoursBetween(birthdate, new DateTime()).getHours();
+					int yearsExpectancy = Integer.parseInt(pref.getString(getString(R.string.expectancyKey), "85"));
+					float hoursExpectancy = (float)Hours.hoursBetween(birthdate, birthdate.plusYears(yearsExpectancy)).getHours();
+					if(hoursExpectancy <= 0){ hoursExpectancy = 1.0f; } //prevent div by zero
+					float percentDead = hoursAlive / hoursExpectancy;
 
 					Paint paint = new Paint();
-                    paint.setStyle(Paint.Style.FILL);
+					paint.setStyle(Paint.Style.FILL);
 
 					paint.setColor(pref.getInt(getString(R.string.bgColorKey), ContextCompat.getColor(getApplicationContext(), R.color.wholesomeTeal)));
 					canvas.drawRect(0, 0, getDesiredMinimumWidth(), getDesiredMinimumHeight(), paint);
 
-                    paint.setColor(pref.getInt(getString(R.string.fgColorKey), ContextCompat.getColor(getApplicationContext(), R.color.blackAsMySOUUUUUUUULLLL)));
-                    canvas.drawRect(0, getDesiredMinimumHeight()-(int)(getDesiredMinimumHeight()*percentDead), getDesiredMinimumWidth(), getDesiredMinimumHeight(), paint);
+					paint.setColor(pref.getInt(getString(R.string.fgColorKey), ContextCompat.getColor(getApplicationContext(), R.color.blackAsMySOUUUUUUUULLLL)));
+					canvas.drawRect(0, getDesiredMinimumHeight()-(int)(getDesiredMinimumHeight()*percentDead),
+                            getDesiredMinimumWidth(), getDesiredMinimumHeight(), paint);
 
-					paint.setTextSize(Float.parseFloat(pref.getString(getString(R.string.textSizeKey), "240")));
-					canvas.drawText(String.format(Locale.US, "%."+pref.getString(getString(R.string.decimalsKey), "4")+"f%%",percentDead*100f), 10, getDesiredMinimumHeight()-(int)(getDesiredMinimumHeight()*percentDead)-10, paint);
+					paint.setTextSize(Float.parseFloat(pref.getString(getString(R.string.progressFontSizeKey), "240")));
+					String progressLabel = String.format(Locale.US, "%."+pref.getString(getString(R.string.decimalsKey), "4")+"f%%",percentDead*100f);
+					canvas.drawText(progressLabel, 10, getDesiredMinimumHeight()-(int)(getDesiredMinimumHeight()*percentDead)-10, paint);
+
+					//draw goals text
+					int statusBarHeight = pref.getInt(getString(R.string.statusBarHeightKey), 100);
+					paint.setTextSize(Float.parseFloat(pref.getString(getString(R.string.goalsFontSizeKey), "75")));
+					String[] goals = pref.getString(getString(R.string.goalsKey), "").split("\n");
+					int lineNumber = 0;
+					float fontHeight = paint.getFontMetrics().descent - paint.getFontMetrics().ascent;
+					for(String line : goals){
+						canvas.drawText(line, 10, statusBarHeight+(0-paint.getFontMetrics().top)+(lineNumber * fontHeight), paint);
+						lineNumber += 1;
+					}
 				}
 				lastDrawTime = System.currentTimeMillis();
 			} finally {
 				if (canvas != null) {
 					getSurfaceHolder().unlockCanvasAndPost(canvas);
-                }
+				}
 			}
 		}
 	}
--- app/src/main/java/com/machinerychorus/lifeprogresswallpaper/SettingsActivity.java
+++ app/src/main/java/com/machinerychorus/lifeprogresswallpaper/SettingsActivity.java
@@ -1,13 +1,16 @@
 package com.machinerychorus.lifeprogresswallpaper;
 
-
 import android.app.WallpaperManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.view.Window;
 import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -37,6 +40,27 @@ public class SettingsActivity extends PreferenceActivity {
             }
         });
         v.addHeaderView(setWallpaperButton);
+
+        /*
+        The wallpaper service needs to know the height of the notification bar so
+        that it can avoid drawing the text underneath the notif bar (looks bad).
+        Apparently the best way to do this is by getting the notification bar
+        height by measuring the window (which can only be done in the activity),
+        then saving it in the preferences so it can be access from the service.
+        https://stackoverflow.com/questions/3044552
+        https://stackoverflow.com/questions/3355367
+        Seems dumb and fragile, but that's never stopped me before!
+        This method also assumes that the status bar is always at the top.
+        We default to 100 so that it (hopefully) won't be covered by the stats bar,
+        even if we don't get the real height.
+        */
+        int statusBarHeight = 100;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+        }
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        pref.edit().putInt(getString(R.string.statusBarHeightKey), statusBarHeight).apply();
     }
 
     /**
